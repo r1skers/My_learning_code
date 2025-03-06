0652757ec92f78501d0126c351a015c24d24185c
r1skers <97279112+r1skers@users.noreply.github.com>
2025-03-07 05:28:55 +0900

    Update BS_Gatesv.v

--- verilog/BS_Gatesv.v
+++ verilog/BS_Gatesv.v
@@ -31,3 +31,70 @@ module top_module(
         out_different[3] = in[3] ^ in[0];
     end
 endmodule
+
+
+//もう少し理解やすいのは
+module top_module( 
+    input  [3:0] in,
+    output [2:0] out_both,      // bits [2:0]
+    output [3:1] out_any,       // bits [3:1]
+    output [3:0] out_different  // bits [3:0]
+);
+
+    // 1) out_both[i] = 1 当 in[i] 和它的左邻居 (更高index) 同时为1
+    //    - out_both[2] = in[2] & in[3]
+    //    - out_both[1] = in[1] & in[2]
+    //    - out_both[0] = in[0] & in[1]
+    assign out_both[0] = in[0] & in[1];
+    assign out_both[1] = in[1] & in[2];
+    assign out_both[2] = in[2] & in[3];
+
+    // 2) out_any[i] = 1 当 in[i] 和它的右邻居 (更低index) 中有任意一个为1
+    //    - out_any[3] = in[3] | in[2]
+    //    - out_any[2] = in[2] | in[1]
+    //    - out_any[1] = in[1] | in[0]
+    //    (不需要 out_any[0])
+    assign out_any[3] = in[3] | in[2];
+    assign out_any[2] = in[2] | in[1];
+    assign out_any[1] = in[1] | in[0];
+
+    // 3) out_different[i] = 1 当 in[i] 和它的左邻居不同
+    //    根据题意，最左边 in[3] 的左邻居 wrap around 到 in[0]
+    //    - out_different[0] = (in[0] ^ in[1])
+    //    - out_different[1] = (in[1] ^ in[2])
+    //    - out_different[2] = (in[2] ^ in[3])
+    //    - out_different[3] = (in[3] ^ in[0])  // wrap around
+    assign out_different[0] = in[0] ^ in[1];
+    assign out_different[1] = in[1] ^ in[2];
+    assign out_different[2] = in[2] ^ in[3];
+    assign out_different[3] = in[3] ^ in[0];
+
+endmodule
+
+
+//最も簡単なのは
+module top_module (
+	input [3:0] in,
+	output [2:0] out_both,
+	output [3:1] out_any,
+	output [3:0] out_different
+);
+
+	// Use bitwise operators and part-select to do the entire calculation in one line of code
+	// in[3:1] is this vector:   					 in[3]  in[2]  in[1]
+	// in[2:0] is this vector:   					 in[2]  in[1]  in[0]
+	// Bitwise-OR produces a 3 bit vector.			   |      |      |
+	// Assign this 3-bit result to out_any[3:1]:	o_a[3] o_a[2] o_a[1]
+
+	// Thus, each output bit is the OR of the input bit and its neighbour to the right:
+	// e.g., out_any[1] = in[1] | in[0];	
+	// Notice how this works even for long vectors.
+	assign out_any = in[3:1] | in[2:0];
+
+	assign out_both = in[2:0] & in[3:1];
+	
+	// XOR 'in' with a vector that is 'in' rotated to the right by 1 position: {in[0], in[3:1]}
+	// The rotation is accomplished by using part selects[] and the concatenation operator{}.
+	assign out_different = in ^ {in[0], in[3:1]};
+	
+endmodule
