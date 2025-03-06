f599fc44ac3ab8f3e2281cd1f035044c9a8afec4
r1skers <97279112+r1skers@users.noreply.github.com>
2025-03-07 07:59:41 +0900

    Update Basic_Adder.v

--- verilog/Basic_Adder.v
+++ verilog/Basic_Adder.v
@@ -1,1 +1,15 @@
-//
+//half adder
+module top_module( 
+    input a, b,
+    output cout, sum );
+    assign{cout,sum}=a+b;
+endmodule
+
+
+
+//full adder
+module top_module( 
+    input a, b, cin,
+    output cout, sum );
+    assign {cout,sum}=a+b+cin;
+endmodule
