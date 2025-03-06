c70fed342fffd08f706f390e8862206329020c0a
r1skers <97279112+r1skers@users.noreply.github.com>
2025-03-07 07:12:01 +0900

    Update BS_Gatesv.v

--- verilog/BS_Gatesv.v
+++ verilog/BS_Gatesv.v
@@ -98,3 +98,19 @@ module top_module (
 	assign out_different = in ^ {in[0], in[3:1]};
 	
 endmodule
+
+
+//If you want a wrap out in different
+module top_module (
+	input [99:0] in,
+	output [98:0] out_both,
+	output [99:1] out_any,
+	output [99:0] out_different
+);
+
+	// See gatesv for explanations.
+	assign out_both = in & in[99:1];
+	assign out_any = in[99:1] | in ;
+	assign out_different = in ^ {in[0], in[99:1]};
+	
+endmodule
