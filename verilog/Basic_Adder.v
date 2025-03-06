557f8915b46c949cc858f7989a0eab9df44d6aa5
r1skers <97279112+r1skers@users.noreply.github.com>
2025-03-07 08:17:57 +0900

    Update Basic_Adder.v

--- verilog/Basic_Adder.v
+++ verilog/Basic_Adder.v
@@ -13,3 +13,26 @@ module top_module(
     output cout, sum );
     assign {cout,sum}=a+b+cin;
 endmodule
+
+
+//3-bit binary ripple carry adder
+module full_adder (
+    input a, b, cin,
+    output sum, cout
+);
+    assign sum = a ^ b ^ cin;
+    assign cout = (a & b) | (b & cin) | (a & cin);
+endmodule
+
+module top_module( 
+    input [2:0] a, b,
+    input cin,
+    output [2:0] cout,
+    output [2:0] sum 
+);
+    // Instantiate three full adders
+    full_adder fa0 (a[0], b[0], cin,   sum[0], cout[0]);
+    full_adder fa1 (a[1], b[1], cout[0], sum[1], cout[1]);
+    full_adder fa2 (a[2], b[2], cout[1], sum[2], cout[2]);
+
+endmodule
