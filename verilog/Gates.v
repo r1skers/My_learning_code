039e209018b03b8037b5ebbd3cc6544cece395ca
r1skers <97279112+r1skers@users.noreply.github.com>
2025-03-06 19:32:17 +0900

    Update Gates.v

--- verilog/Gates.v
+++ verilog/Gates.v
@@ -6,5 +6,9 @@ module Gates(
     //assign out=1'b0;------>VSS(电源负极)GND(接地)
     //assign out=in1&in2;---->AND(与门)
     //assign out=in1|in2;---->OR(或门)
-    //
+    //assign out=~in1;------->INVERTER(非门)
+    //assign out=~(in1|in2);->NOR(或非门)
+    //assign out=in1^in2;---->XOR(异或门)
+    //assign out=~(in1&in2);->NAND(与非门)
+    
 endmodule
