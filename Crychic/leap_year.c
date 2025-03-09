29f62bbfcbc95aa9e29d9587d8d6a051e8539b78
r1skers <97279112+r1skers@users.noreply.github.com>
2025-03-10 03:08:47 +0900

    更新 leap_year.c

--- Crychic/leap_year.c
+++ Crychic/leap_year.c
@@ -1,1 +1,16 @@
-//
+//leap.h
+#ifndef LEAP_H
+#define LEAP_H
+
+#include <stdbool.h>
+
+bool leap_year(int year);
+
+#endif
+
+
+//leap.c
+#include "leap.h"
+bool leap_year(int year){
+    return (year%4==0&&year%100!=0)||(year%400==0);
+}
