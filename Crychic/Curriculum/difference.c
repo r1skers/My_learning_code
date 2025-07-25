93db819f4185a96b3e90f39ec9d9b3d6909d5605
r1skers <97279112+r1skers@users.noreply.github.com>
2025-07-25 10:18:36 +0900

    Add files via upload
    check the number of "0" and "1" in a file

---
+++ Crychic/Curriculum/difference.c
@@ -0,0 +1,39 @@
+//0 と 1 のみからなるファイルに含まれる「0 の個数」と「１の個数」の差の絶対値を出力する実行ファイル
+difference を作成せよ．さらに、WebClass で提示されるファイルについて、その差を解答せよ。
+
+
+
+
+#include<stdio.h>
+void count(int a)
+{
+  if(a<0)
+    {
+      printf("%d",-a);
+    }
+  else if(a>0)
+    {
+      printf("%d",a);
+    }
+  return;
+}
+int main(void){
+  int a,sum0=0,sum1=0;
+  char *filename="array.dat";
+
+  FILE *file=fopen(filename,"r");
+  while(fscanf(file,"%d",&a)==1)
+    {
+    if(a==0)
+      {
+	sum0++;
+      }
+    else if(a==1)
+      {
+	sum1++;
+      }
+  }
+  fclose(file);
+  count(sum0-sum1);
+    return 0;
+}
